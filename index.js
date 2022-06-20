3cea85e1073526733112a0c113f29091dc00e6c6
Kelvin <kmwendwa476@gmail.com>
2022-06-20 05:55:19 +0300

    commit tree 26d9cfcbf3b8c6776ad0d2c540f239c4cad74f92

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, nameString){
+    return drivers.filter(function(string){
+        return((string.toLowerCase()) === nameString.toLowerCase())
+    })
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function(pLetter){
+        if((pLetter.indexOf(letters)) === 0){
+            return true;
+        }else if (pLetter.indexOf(letters)>0){
+            return false;
+        }
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(element){
+        return element.name === string;
+    })
+}
